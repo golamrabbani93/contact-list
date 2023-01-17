a3e6196fcbc09507abd04b19122f91585ee4eccb
golamrabbani93 <golamrabbani9373@gmail.com>
2023-01-17 22:05:38 +0600

    import customer contact list from API

--- src/components/UserList/UserList.js
+++ src/components/UserList/UserList.js
@@ -1,9 +1,47 @@
-import React from 'react';
+import React, {useState, forwardRef} from 'react';
+import {useQuery} from '@tanstack/react-query';
 import {useNavigate} from 'react-router';
-
+import MaterialTable from '@material-table/core';
+import AddBox from '@material-ui/icons/AddBox';
+import ArrowDownward from '@material-ui/icons/ArrowDownward';
+import Check from '@material-ui/icons/Check';
+import ChevronLeft from '@material-ui/icons/ChevronLeft';
+import ChevronRight from '@material-ui/icons/ChevronRight';
+import Clear from '@material-ui/icons/Clear';
+import DeleteOutline from '@material-ui/icons/DeleteOutline';
+import Edit from '@material-ui/icons/Edit';
+import FilterList from '@material-ui/icons/FilterList';
+import FirstPage from '@material-ui/icons/FirstPage';
+import LastPage from '@material-ui/icons/LastPage';
+import Remove from '@material-ui/icons/Remove';
+import SaveAlt from '@material-ui/icons/SaveAlt';
+import Search from '@material-ui/icons/Search';
+import ViewColumn from '@material-ui/icons/ViewColumn';
 const UserList = () => {
+	const tableIcons = {
+		Add: forwardRef((props, ref) => <AddBox {...props} ref={ref} />),
+		Check: forwardRef((props, ref) => <Check {...props} ref={ref} />),
+		Clear: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
+		Delete: forwardRef((props, ref) => <DeleteOutline {...props} ref={ref} />),
+		DetailPanel: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
+		Edit: forwardRef((props, ref) => <Edit {...props} ref={ref} />),
+		Export: forwardRef((props, ref) => <SaveAlt {...props} ref={ref} />),
+		Filter: forwardRef((props, ref) => <FilterList {...props} ref={ref} />),
+		FirstPage: forwardRef((props, ref) => <FirstPage {...props} ref={ref} />),
+		LastPage: forwardRef((props, ref) => <LastPage {...props} ref={ref} />),
+		NextPage: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
+		PreviousPage: forwardRef((props, ref) => <ChevronLeft {...props} ref={ref} />),
+		ResetSearch: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
+		Search: forwardRef((props, ref) => <Search {...props} ref={ref} />),
+		SortArrow: forwardRef((props, ref) => <ArrowDownward {...props} ref={ref} />),
+		ThirdStateCheck: forwardRef((props, ref) => <Remove {...props} ref={ref} />),
+		ViewColumn: forwardRef((props, ref) => <ViewColumn {...props} ref={ref} />),
+	};
 	const navigate = useNavigate();
 	const token = window.localStorage.getItem('Token');
+	const [user, setUser] = useState();
+	console.log('🚀🚀: UserList -> user', user);
+
 	fetch('http://localhost:5000/contactlist', {
 		method: 'POST',
 		headers: {
@@ -13,6 +51,9 @@ const UserList = () => {
 	})
 		.then((res) => res.json())
 		.then((data) => {
+			if (data.status === 200) {
+				!user && setUser(data.data);
+			}
 			if (data.data === 'forbidden access') {
 				navigate('/signin');
 			}
@@ -20,9 +61,41 @@ const UserList = () => {
 				alert(data.error);
 			}
 		});
+
+	const {data: customersData = [], isLoading} = useQuery({
+		queryKey: [user],
+		queryFn: async () => {
+			const res = await fetch(`http://localhost:5000/customerlist?email=${user?.email}`);
+			const data = await res.json();
+			return data;
+		},
+	});
+	if (isLoading) {
+		return <div>Loading.............</div>;
+	}
+	console.log(customersData);
+	const columns = [
+		{
+			title: 'Name',
+			field: 'name',
+		},
+		{
+			title: 'Email',
+			field: 'email',
+		},
+		{
+			title: 'Phone',
+			field: 'Phone',
+		},
+	];
 	return (
 		<div>
-			<h2>UserList</h2>
+			<MaterialTable
+				icons={tableIcons}
+				title={'Customer Details'}
+				columns={columns}
+				data={customersData}
+			></MaterialTable>
 		</div>
 	);
 };
